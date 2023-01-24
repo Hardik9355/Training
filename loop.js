e295fffe2cb7c9fa8353e66b2dee18131bb8a607
HardikGRG <Hardik Garg>
2023-01-24 02:00:23 -0500

    first commit

---
+++ loop.js
@@ -0,0 +1,13 @@
+// {
+//     const arr =["hardik","garg","saksham","jindal"]
+//     for(let i =0 ; i<arr.length;i++)
+//     {
+//         console.log(arr)
+//     }
+// }
+{
+    const arr =["hardik","garg","saksham","jindal"]
+    for (i in arr){
+        console.log(arr)
+    }
+}
