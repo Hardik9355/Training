62f4c4805e7b8deed1153599de2d77939a1a729e
HardikGRG <Hardik Garg>
2023-01-25 00:17:40 -0500

    updating

--- destructuring.js
+++ destructuring.js
@@ -1,8 +1,24 @@
-let a =["Hardik"  , "Jai" , "Saksham"]
+// const obj ={ aa:"Hardik"  , bb:"Jai" , cc:"Saksham", dd: "paras", ee:"chitresh"}
 
-let [b, ...rest] = a
-console.log(b)
-console.log(rest) 
-let c = ["Garg","Goyal","Jindal"]
-const d = [...a,...c]
-console.log(d)
+// const {aa,bb} = obj;
+// console.log(aa,bb)
+
+// console.log(b)
+// console.log(rest) 
+// let c = ["Garg","Goyal","Jindal"]
+// const d = [...a,...c]
+
+// console.log(d)
+// const e = (item,index) => {
+// const a =["Hardik", "Saksham","Jai","Chitresh"]
+// const [aa,bb,c,d] = a
+// console.log(c,d,aa)
+const a =["Hardik"]
+const b = (num)=>{
+    return num+ " " +"Garg"
+}
+const d = a.map(b)
+console.log(d)   
+// let [aa,bb]= a
+// console.log(aa,bb)
+// }
