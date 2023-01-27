f635f9d43c3fb1117a1a64f782c9f7999367361c
HardikGRG <Hardik Garg>
2023-01-27 02:32:18 -0500

    First commit

---
+++ test.js
@@ -0,0 +1,11 @@
+
+function test(){
+    document.getElementById("2").innerHTML="Welcome Hardik";
+    
+}
+function test1(){
+    document.getElementById("3").addEventListener("click",alert("hello"));
+}
+function test2(val){
+    alert("Your name is :"+val);
+}
