fff58ec99a859396ff957c6bdf0a99986831e983
HardikGRG <Hardik Garg>
2023-01-26 23:46:11 -0500

    updating

--- async.js
+++ async.js
@@ -4,8 +4,8 @@ import fetch from "node-fetch";
 
 try{
 async function result(){
-    // console.log("execution start")
-    const response  = await fetch("https://bobbyhadz.com/blog/javascript-referenceerror-fetch-is-not-defined");
+    console.log("execution start")
+    const response  = await fetch("https://api.github.com/users/Hardik9355");
     const data = await response.json()
     console.log(data)
     console.log("execution end")
@@ -15,12 +15,12 @@ result()
 catch{
     console.log("url not found")
 }
-// function resolve (){
-//     return new Promise(result =>{
-//         setTimeout(() => {
-//             result('resolved');
+function resolve (){
+    return new Promise(result =>{
+        setTimeout(() => {
+            result('resolved');
             
-//         }, 5000);
+        }, 5000);
 
-//     });
-// }
+    });
+}
